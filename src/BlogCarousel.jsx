3d5d0a59512d2c8cb257a92a002b76004bcd5258
Gilbert Tan <57788135+gbtan1991@users.noreply.github.com>
2023-11-15 18:12:17 +0800

    continue with the tutorial

--- src/BlogCarousel.jsx
+++ src/BlogCarousel.jsx
@@ -1,13 +1,13 @@
 import Blogs from './Blogs.jsx';
 
-const BlogCarousel = () => {
+const BlogCarousel = ({ blogs, title }) => {
   
 
       return (
     <div>
       <div className="px-10 py-5">
-      <h2 className="text-2xl font-mono ">Recent blogs</h2>
-      <Blogs />
+      <h2 className="text-2xl font-mono ">{ title }</h2>
+      <Blogs blogs={blogs} />
       
 
     </div>
--- src/Blogs.jsx
+++ src/Blogs.jsx
@@ -1,49 +1,25 @@
-import { useState } from 'react'
+const Blogs = ({ blogs }) => {
 
-const Blogs = () => {
-    const [blogs, setBlogs] = useState([
-        {
-            id: 1,
-            title: 'Getting Started with React',
-            author: 'John Doe',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
-            date: '2023-01-01',
-            imageUrl: 'https://images.pexels.com/photos/11035471/pexels-photo-11035471.jpeg',
-          },
-          {
-            id: 2,
-            title: 'CSS Best Practices',
-            author: 'Jane Smith',
-            content: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...',
-            date: '2023-02-15',
-            imageUrl: 'https://images.pexels.com/photos/256502/pexels-photo-256502.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-          },
-          {
-            id: 3,
-            title: 'JavaScript Fundamentals',
-            author: 'Bob Johnson',
-            content: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...',
-            date: '2023-03-10',
-            imageUrl: 'https://images.pexels.com/photos/879109/pexels-photo-879109.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-          },
-  ]);
-  return( 
-      <div className='flex flex-row gap-5'>
-        {blogs.map((blog) => (
-            
-            <div key={blog.id} className='font-mono w-96 shadow-retro text-white bg-zinc-950 shadow-teal-500'>
-                <img src={blog.imageUrl} className='w-96 h-72'/>
-                <div className='p-5'>
-                <h2 className='text-2xl text-teal-500'>{blog.title}</h2>
-                <p className='italic'>{blog.author}</p>
-                <p className='text-justify'>{blog.content}</p>
-                <p className='text-white hover:text-teal-500'>Read More..</p>
-                </div>
+  return (
+    <div className="flex flex-row gap-5">
+      {blogs.map((blog) => {
+        return (
+          <div
+            key={blog.id}
+            className="font-mono w-96 shadow-retro text-white bg-zinc-950 shadow-teal-500"
+          >
+            <img src={blog.imageUrl} alt={blog.imageAlt} className="w-96 h-72" />
+            <div className="p-5">
+              <h2 className="text-2xl text-teal-500">{blog.title}</h2>
+              <p className="italic">{blog.author}</p>
+              <p className="text-justify">{blog.content}</p>
+              <p className="text-white hover:text-teal-500">Read More..</p>
             </div>
-            
-        ))}
-      </div>
-  
-  )};
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Blogs;
