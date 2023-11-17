c00a06d263c010c75128c12cc9175e52e1de14bf
Gilbert Tan <57788135+gbtan1991@users.noreply.github.com>
2023-11-17 13:40:56 +0800

    create error handling

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,39 +1,47 @@
 import React, { useState, useEffect } from "react";
 import Header from "./Header.jsx";
 import BlogCarousel from "./BlogCarousel.jsx";
+import Error404 from "./Error404.jsx";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        if(!res.ok){
+          throw Error("Couldn't connect to the resources");
+        }
+        return res.json();
+        
+      })
+      .then((data) => {
+        setBlogs(data);
+        setLoading(false);
+        setError(null);
+        
+      })
+      .catch(error => {
+        console.log("Error fetching data:", error);
+        setLoading(false);
+        setError(error.message)
+      }, 1000);
+  }, [setError]);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
 
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-        setLoading(false);
-      })
-        
-      
-
-      .catch((error) => {
-        console.log("Error fetching data:", error);
-        setLoading(false);
-      });
-  }, []);
 
   return (
     <div>
       <Header />
-        { loading && <div>Loading...</div>}
+      { error &&  <Error404 error={error}/>}
+      { loading && <div>Loading...</div>}
       {blogs && (
         <BlogCarousel 
           blogs={blogs}
