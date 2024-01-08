940772a265ff89eb2393f1dcf1c0fad6adaf9b1c
Gilbert Tan <57788135+gbtan1991@users.noreply.github.com>
2024-01-08 14:17:00 +0800

    recreating the whole vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
-import Home from "./Home.jsx";
-import Header from "./Header.jsx";
-import { Routes, Route } from "react-router-dom";
-import Layout from "./Layout.jsx";
-import About from "./About.jsx";
-import Create from "./Create.jsx"
+import react from 'react'
 
+const App = () => {
 
-export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Create" element={<Create />}/>
-      </Route>
-    </Routes>
-  );
+    <div>
+      Welcome
+    </div>
+  )
+
 }
+
+export default App
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      boxShadow: {
-        retro: "8px 8px 0 0",
-      },
-
-      colors: {
-        cream: "#ffefc9",
-        lightCream: "#fff7e9",
-        yellow: "#ffcb04",
-        darkGray: "#494a4a",
-        baseGray: "#5e5e5e",
-        orange: "#ff5d25",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
