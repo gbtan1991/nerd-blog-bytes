24567066565787be553edd296908d41f5a8a9857
Gilbert Tan <57788135+gbtan1991@users.noreply.github.com>
2024-01-22 16:02:17 +0800

    working on the useEffect custom Hook

--- src/components/BlogsList.jsx
+++ src/components/BlogsList.jsx
@@ -1,53 +1,27 @@
-
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { json } from "react-router-dom";
+import React from "react";
+import useFetch from "../useFetch";
 
 const BlogsList = () => {
-  const [jsonData, setJsonData] = useState(null);
-  const [hasData, setHasData] = useState(false);
+  const {
+    data,
+    isPending,
+    error,
+  } = useFetch(import.meta.env.VITE_REACT_APP_JSON_DATA_URL);
 
-  useEffect(() => {
-    // Fetch JSON data from the GitHub Gist using Axios
-    const fetchData = async () => {
-      try {
-        const { data } = await axios.get(
-          import.meta.env.VITE_REACT_APP_JSON_DATA_URL
-        );
-        setJsonData(data.blogs);
-        setHasData(true);
-      } catch (error) {
-        console.error("Error fetching JSON:", error);
-      }
-    };
-    
-    fetchData();
-  }, []); // Empty dependency array means this effect runs once after the initial render
-  
-  console.log(jsonData)
   return (
     <div className="text-white mt-[56px] lg:mt-[121px]">
       <h1>Blogs</h1>
+      {isPending && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      {data && 
       <ul>
-      {hasData ? (
-        jsonData.map((blog, index) => (
-          <li key={index}>
-            <h2>{blog.title}</h2>
-            <h3>{blog.author.name}</h3>
-            <p>{blog.publishedDate}</p>
-            {blog.tags.map((tag, tagIndex) => (
-              <div key={tagIndex}>
-                <span>{tag}</span>
-              </div>
+        {data.BlogsList.map((blog, index) =>
+        <li key={index}>
+          <h2>{blog.title}</h2>
+        </li>)}
+      </ul>
 
-            ))}
-            
-          </li>
-        ))
-      ) : 
-        <p>Loading</p>
       }
-</ul>
     </div>
   );
 };
