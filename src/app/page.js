a1432dd4f29dc46ee6df4e8b7c7c65f68a2b4c0f
Gilbert Tan <57788135+gbtan1991@users.noreply.github.com>
2024-01-26 16:48:02 +0800

    Create the hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
+import Hero from './components/Hero'
 
 
 
 
 export default function Home() {
   return (
-    <div className='bg-zinc-900 h-screen overflow-hidden'>
-      test
+    <div className='bg-zinc-900 h-screen overflow-auto relative'>
+      <Hero />
     </div>
   )
 }
