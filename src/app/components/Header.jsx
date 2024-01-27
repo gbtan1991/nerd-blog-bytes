7c808ec1b3fd842d7358138cbd36c30b0edc50c4
Gilbert Tan <57788135+gbtan1991@users.noreply.github.com>
2024-01-27 15:34:33 +0800

    Fix bugs after the merge

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,13 +1,21 @@
 "use client"
 import React from 'react'
-import TopBar from './TopBar'
+import Logo from './Logo'
+import NavDesktop from './NavDesktop'
+import NavMobile from './NavMobile'
 
 
 
 const Header = () => {
   return (
-    <header>
-        <TopBar />
+    <header className='fixed w-full top-0 left-0 bg-zinc-900 border-b border-zinc-700' >
+        
+        <nav className='contianer flex items-center justify-between lg:justify-evenly  px-5 py-1 lg:py-5'>
+            <Logo />
+            <NavMobile />
+            <NavDesktop />
+
+        </nav>
     </header>
   )
 }
